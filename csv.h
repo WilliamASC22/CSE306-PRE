65247496a4c230cd7f70f77ab88237166d3edf6f
Emma G Ressman <emmaress@buffalo.edu>
2025-09-18 11:06:03 -0400

    added header and tests files

---
+++ csv.h
@@ -0,0 +1,19 @@
+/*
+  CSV file operations
+*/
+
+#ifndef CSV
+#define CSV
+
+int countColumns(FILE *currFile);
+int countRows(FILE *currFile);
+void printResult(double num);
+int allDigits(char *str);
+int getColIndex(char *colName, FILE *currFile);
+double maxField(int colIndex, FILE *currFile);
+double minField(int colIndex, FILE *currFile);
+double meanField(int colIndex, FILE *currFile);
+int findRecords(bool h, int colIndex, char *value, FILE *currFile, char **matches);
+
+
+#endif
